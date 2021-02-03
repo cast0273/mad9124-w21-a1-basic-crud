981f3c2c8d3d1dc42af9b4e58db18aa0a69527a8
JC Castagne <74418183+JCcastagne@users.noreply.github.com>
2021-02-02 19:13:47 -0500

    populate js data objects

---
+++ data/course.js
@@ -0,0 +1,10 @@
+const course = [
+  {
+    id: 1,
+    code: 9124,
+    title: 'Mobile API Development',
+    description: 'Students enhance their JavaScript skills to become productive with Full-stack development',
+    url: 'https://mad9124.github.io/W2021/overview/'
+  }
+]
+
---
+++ data/student.js
@@ -0,0 +1,9 @@
+const student = [
+  {
+    id: 41001742,
+    firstName: 'Jean-Christophe Castagne',
+    lastName: 'student',
+    nickName: 'JC',
+    email: 'cast0273@algonquinlive.com'
+  }
+]
