3a5f8110144eec981076cd02206446fc0876514e
JC Castagne <74418183+JCcastagne@users.noreply.github.com>
2021-02-02 20:10:09 -0500

    start build of main module app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+'use strict'
+// load dependencies
+const express = require('express')
+const studentRouter = require('./routes/studentRouter.js')
+const courseRouter = require('./routes/courseRouter.js')
+
+// create the express app
+const app = express()
+
+// configure express middleware
+app.use(express.json())
+
+// define routes
+app.use('/api/student', studentRouter)
+app.use('/api/course', courseRouter)
+
+
+
+// start listening for HTTP requests
+const port = process.env.port || 3030
+app.listen(port, () => console.log(`Server listening on port ${port} ...`))
